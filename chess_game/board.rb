bd2bbed6bdd376e8f52ce2d2c6fb911434784dd9
Jonnatan <jonnatano94@gmail.com>
2018-10-30 12:14:03 -0700

    Create board, display and cursor file

--- chess_game/board.rb
+++ chess_game/board.rb
@@ -1,71 +1,89 @@
+require "byebug"
+require "singleton"
+# require "colorized"
+
 #Updated with comment
 class Board
-  attr_accessor :piece
 
-  def default_grid
-    grid = Array.new(8) {Array.new(8)}
+  attr_reader :piece, :grid
+
+  def initialize
+    @grid = Array.new(8) {Array.new(8)}
+    @sentinel = NullPiece.instance
+    set_up_board
+  end
+
+  def [](pos)
+    # debugger
+    row, col = pos
+    @grid[row][col]
+  end
+
+  def []=(pos, value)
+    row, col = pos
+    @grid[row][col] = value
+  end
+
+
+  def move_piece(start_pos, end_pos)
+    # debugger
+    if self[start_pos].is_a?(NullPiece)
+      raise "There is no piece to move at that starting position"
+    end
+    if !self[end_pos].is_a?(NullPiece)
+      raise "The piece canot be moved to that end position"
+    end
+    update_board(start_pos, end_pos)
+  end
+
+  def update_board(old_pos, new_pos)
+    old_piece = self[old_pos]
+    self[old_pos] = @sentinal
+    self[new_pos] = old_piece
   end
 
   def set_up_board
-    rows_to_set = [0, 1, 6, 7]
-    @grid.each_with_index do |row, idx|
-      row.each_with_index do |col, idx2|
-        if rows_to_set.include?(idx)
-          pos = @piece
-        else
-          pos = nil
+    occupy_rows = [0, 1, 6, 7]
+    @grid.each_with_index do |row,idx|
+      if occupy_rows.include?(idx)
+        row.each_index do |idx2|
+          @grid[idx][idx2] = Piece.new('p')
+        end
+      else
+        row.each_index do |idx2|
+          # debugger
+          @grid[idx][idx2] = @sentinel
         end
       end
     end
-    @grid
   end
-
-  def initialize
-    @grid = self.default_grid.set_up_board
-  end
-
-  def pos=(value)
-    pos = row, col
-    value = @piece
-  end
-
-  def move_piece(start_pos, end_pos)
-    #check whether the piece can move there
-    #check if the spot is occupied by its team piece
-    #move piece if two cases pass else raise exception
-    #render board with new piece position
-    if !start_pos.nil?
-      raise "0"
-    elsif !end_pos.nil? && end_pos.include?(teampiece)
-      raise "1"
-    else
-      update_board(end_pos)
-    end
-  end
-
-  def update_board(pos)
-  end
-
-  def start_pos
-  end
-
-  def occupied?
-  end
-
-  # def gets_move
-  #   gets.chomp.split(",")     #In the game itself
-  # end
-  #
 end
 
 class Piece
   def initialize(piece)
     @piece = piece
   end
+
+  def inspect
+    "P"
+  end
 end
 
 class NullPiece < Piece
-  def initialize(piece)
-    super(piece) = nil
+  include Singleton
+
+  def initialize
   end
+
+  def inspect
+    "X"
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  board = Board.new()
+  p board.grid
+  board.move_piece([3,0], [4,0])
+  p "move after"
+  p board.grid
 end
